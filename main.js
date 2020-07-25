e83526a75b36262f16215bcca1d010ca8db16294
Ahmed Salama <eng.ahd746@gmail.com>
2020-07-24 21:44:45 -0400

    Phase two completed

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+"use strict";
 // Get the elements
 let input = document.querySelector('input');
 let root = document.getElementById('root');
@@ -12,13 +13,13 @@ addBtn.addEventListener('click', (e) => {
 
   //create label with text of input
   let text = document.createTextNode(input.value);
-  let label = document.createElement('label')
+  let label = document.createElement('label');
   label.appendChild(text);
 
   //create checkbox and add its attributes and events
   let checkbox = document.createElement('input');
-  checkbox.setAttribute('type', 'checkbox')
-  checkbox.setAttribute('class', 'mr-2')
+  checkbox.setAttribute('type', 'checkbox');
+  checkbox.setAttribute('class', 'mr-2');
 
   //Event: when checkbox clicked todo goes down and line-through property added and sound effect, 
   checkbox.addEventListener('click', (e) => {
@@ -36,20 +37,20 @@ addBtn.addEventListener('click', (e) => {
   let delBtn = document.createElement('button');
   text = document.createTextNode('X');
   delBtn.appendChild(text);
-  delBtn.setAttribute('class', 'del ml-2')
-  delBtn.setAttribute('title', 'delete')
+  delBtn.setAttribute('class', 'del ml-2');
+  delBtn.setAttribute('title', 'delete');
   //Event: on click remove the todo
-  delBtn.addEventListener('click', (e) => { e.target.parentElement.remove() })
+  delBtn.addEventListener('click', (e) => { e.target.parentElement.remove() });
 
   //create div which contain all element and append it to the root div 
   let div = document.createElement('div');
   div.appendChild(checkbox);
   div.appendChild(label);
-  div.appendChild(delBtn)
+  div.appendChild(delBtn);
   root.appendChild(div);
 
   //clear the input 
-  input.value = ''
+  input.value = '';
 })
 
 // Event: on clear button to clear the input todo
@@ -59,3 +60,51 @@ clearBtn.addEventListener('click', (e) => {
 })
 
 
+//Phase two add todos API as example for user 
+//fetch data 
+let fetchTodos = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+  const todos = await response.json();
+
+  // loop through data and add each todo to the DOM 
+  todos.map(item => {
+    //create label with text of input
+    let text = document.createTextNode(item.title);
+    let label = document.createElement('label');
+    label.appendChild(text);
+    //create checkbox and add its attributes and events
+    let checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.setAttribute('class', 'mr-2');
+
+    //Event: when checkbox clicked todo goes down and line-through property added and sound effect, 
+    checkbox.addEventListener('click', (e) => {
+      e.target.nextElementSibling.getAttribute('class') === 'underline' ?
+        e.target.nextElementSibling.removeAttribute('class', 'underline') :
+        (
+          e.target.nextElementSibling.setAttribute('class', 'underline'),
+          e.target.parentElement.parentElement.appendChild(e.target.parentElement),
+          document.getElementById("audio").play()
+        )
+
+    })
+
+    //Create delete button and add its events
+    let delBtn = document.createElement('button');
+    text = document.createTextNode('X');
+    delBtn.appendChild(text);
+    delBtn.setAttribute('class', 'del ml-2');
+    delBtn.setAttribute('title', 'delete');
+    //Event: on click remove the todo
+    delBtn.addEventListener('click', (e) => { e.target.parentElement.remove() });
+
+    //create div which contain all element and append it to the root div 
+    let div = document.createElement('div');
+    div.appendChild(checkbox);
+    div.appendChild(label);
+    div.appendChild(delBtn);
+    root.appendChild(div);
+  });
+}
+// Call function
+fetchTodos();
